874cb2974ec27615051f6b3d133aa2895f177b74
AnushkaAmte <anushkaamte@gmail.com>
2021-03-17 01:47:21 +0530

    added auto login logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:carewise/providers/auth.dart';
-import 'package:carewise/screens/edit_medicine.dart';
-import 'package:carewise/widgets/tabbar_screen.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'screens/login_page.dart';
+//import 'screens/login_page.dart';
+import 'screens/auth_screen.dart';
+import 'screens/edit_medicine.dart';
+import 'screens/splash_screen.dart';
+import 'widgets/tabbar_screen.dart';
 import 'screens/medicine_detail.dart';
 import 'screens/logbook_screen.dart';
 import 'screens/inventory.dart';
@@ -25,10 +28,17 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (context) => MedicineList(),
+          create: (context) => Auth(),
         ),
-        ChangeNotifierProvider(
-          create: (context) => LogBookProvider(),
+        ChangeNotifierProxyProvider<Auth, MedicineList>(
+          update: (context, auth, previousMeds) => MedicineList(auth.token,
+              auth.userID, previousMeds == null ? [] : previousMeds.items),
+          create: (context) => MedicineList(null, null, []),
+        ),
+        ChangeNotifierProxyProvider<Auth, LogBookProvider>(
+          update: (context, auth, previousLogs) => LogBookProvider(auth.token,
+              auth.userID, previousLogs == null ? [] : previousLogs.items),
+          create: (context) => LogBookProvider(null, null, []),
         ),
         ChangeNotifierProvider(
           create: (context) => MedicineProvider(),
@@ -39,34 +49,33 @@ class MyApp extends StatelessWidget {
         StreamProvider<FirebaseUser>.value(
           value: FirebaseAuth.instance.onAuthStateChanged,
         ),
-        /* ChangeNotifierProvider(
-          create: (context) => Auth(),
-        ), */
       ],
-      child: MaterialApp(
-        title: 'CareWise',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          buttonColor: Color(0xA846A0),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: StreamBuilder(
-          stream: FirebaseAuth.instance.onAuthStateChanged,
-          builder: (ctx, userSnapshot) {
-            if (userSnapshot.hasData) {
-              return TabBarScreen();
-            }
-            return LoginPage();
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+          title: 'CareWise',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.green,
+            buttonColor: Color(0xA846A0),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: auth.isAuth
+              ? TabBarScreen()
+              : FutureBuilder(
+                  future: auth.tryAutoLogIn(),
+                  builder: (context, authResult) =>
+                      authResult.connectionState == ConnectionState.waiting
+                          ? SplashScreen()
+                          : AuthScreen()),
+          routes: {
+            MedicineDetail.routeName: (ctx) => MedicineDetail(),
+            LogbookScreen.routeName: (ctx) => LogbookScreen(),
+            Inventory.routeName: (ctx) => Inventory(),
+            AddMedicine.routeName: (ctx) => AddMedicine(),
+            EditMedicine.routeName: (ctx) => EditMedicine(),
+            AuthScreen.routeName: (ctx) => AuthScreen(),
           },
         ),
-        routes: {
-          MedicineDetail.routeName: (ctx) => MedicineDetail(),
-          LogbookScreen.routeName: (ctx) => LogbookScreen(),
-          Inventory.routeName: (ctx) => Inventory(),
-          AddMedicine.routeName: (ctx) => AddMedicine(),
-          EditMedicine.routeName: (ctx) => EditMedicine(),
-        },
       ),
     );
   }
